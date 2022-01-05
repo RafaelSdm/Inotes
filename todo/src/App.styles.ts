5bb80585ff9b5d73d42c56c1bdd47eadd5d623e5
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-05 15:54:17 -0300

    initial done

---
+++ todo/src/App.styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+
+    background-color: #17181F;
+    color: #797A81;
+    min-height: 100vh;
+`
+
+export const Area = styled.div`
+
+    margin: 0 auto;
+    max-width: 980px;
+    padding: 10px;
+
+
+`;
+
+
+export const header = styled.h1`
+
+    margin:0;
+    padding:0;
+    text-align:center;
+    color: #fff;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+
+`
