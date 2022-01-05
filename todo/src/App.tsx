ddb4a3cf43721bae7cc71a3a8160736338949515
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-01-05 17:22:23 -0300

    project done

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,6 +1,31 @@
 import * as C from './App.styles'
+import {useState} from 'react'
+import {Item} from './types/item'
+import {ListItem} from './components/ListItem';
+import {AddArea} from './components/AddArea'
+
 
 const App = () =>{
+
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'comprar um pao na padaria', done: false},
+    {id: 2, name: 'comprar frutas na padaria', done: true}
+  ])
+
+
+  const handleAddTask = (taskName: string) => {
+
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+
+    });
+
+    setList(newList)
+
+  }
 
   return (
 
@@ -10,6 +35,14 @@ const App = () =>{
         <C.header>
           Inotes
         </C.header>
+
+       <AddArea onEnter={handleAddTask}/>
+
+        {list.map((item, index)  => (
+         
+         <ListItem key={index} item={item}/>
+         
+        ))}
       </C.Area>
 
     </C.Container>
---
+++ todo/src/components/AddArea/index.tsx
@@ -0,0 +1,40 @@
+import * as C from './styles'
+import {useState, KeyboardEvent} from 'react'
+
+
+
+type Props ={
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea  =  ({onEnter}: Props) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleKeyUp = (e: KeyboardEvent) =>{
+        console.log(e.code);
+        if(e.code ==='Enter' && inputText !== ''){
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
+    return (
+
+        <C.Container>
+           <div className='image'>+</div>
+           <input 
+           type="text" 
+           name="" 
+           id=""
+           placeholder='Adicionar uma tarefa'
+           value={inputText}
+           onChange={e => setInputText(e.target.value)} 
+           onKeyUp={handleKeyUp}
+           
+           
+           /> 
+        </C.Container>
+        
+
+
+    );
+}
